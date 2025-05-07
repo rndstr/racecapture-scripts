f605e981e83e7def8af9b5bfeb69a2c36ed28945
RS <roli@schilter.me>
2025-05-06 19:10:16 -0700

    Add dbg() method

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
+DEBUG = true
 TICKS = 50
-setTickRate(TICKS)
 
+function dbg(str)
+  if DEBUG then print(str) end
+end
+
+setTickRate(TICKS)
 function onTick()
   -- tick_mock()
   -- tick_vminmax_simple()
