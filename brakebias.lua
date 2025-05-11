cf2408217f3fb2a08d2d5df6518521d042216aeb
Roli Schilter <roli@schilter.me>
2025-05-11 13:44:31 -0700

    brakebias: only update if we are braking

--- brakebias.lua
+++ brakebias.lua
@@ -1,7 +1,9 @@
-bbid = addChannel('BrakeBias', 50, 1, 0, 100, '%')
+bbid = addChannel('BrakeBias', 5, 1, 0, 100, '%')
 
 function tick_brakebias()
   local f = getChannel('BrakeF')
   local b = getChannel('BrakeR')
-  setChannel(bbid, f/(f+b)*100)
+  -- only update when brake applied
+  if b ~= 0 then setChannel(bbid, f/(f+b)*100) end
 end
+
