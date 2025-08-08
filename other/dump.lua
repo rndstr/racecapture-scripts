35cb1c085ed5263c0ae47e73dc54fdb1b72d3ba9
RS <roli@schilter.me>
2025-08-07 19:40:50 -0700

    dump: add script to dump signals and channels for reverse engineering

---
+++ other/dump.lua
@@ -0,0 +1,49 @@
+--------------------------------------------
+-- dump: Dumps configured can messages.
+
+-- Signals to reverse engineer.
+local signals = {
+  {name="ABSASC", id=371},
+}
+
+-- Accompanying values to compare to.
+local channels = {
+  -- Newly calculated basd on signal.
+  "ABS", "ASC", "BrakeState",
+  -- Known good values to compare to.
+  "Speed", "Brake", "BrakeF", "BrakeR",
+}
+
+function dumpSignals()
+  repeat 
+    id, ext, data = rxCAN(0, 1000)
+    if id ~= nil then
+      for unused, signal in pairs(signals) do 
+        if signal.id == id then
+          print('\n[' ..id ..']'..signal.name..', ')
+          for i = 1,#data do
+            print(data[i] ..', ')
+          end
+        end
+      end
+    end
+  until id == nil
+    println('----- END -----')
+end
+
+function dumpChannels()
+  for unused, channel in ipairs(channels) do
+    val = getChannel(channel)
+    print('\n'..channel..', ')
+    if val == nil then print('nil')
+    else print(val)
+    end
+  end
+end
+ 
+function onTick()
+  dumpSignals()
+  dumpChannels()
+end
+
+setTickRate(30)
